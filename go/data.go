4740c7be90e3151fc21d40fc72532b1dc1a6b4ae
Erik Brown <iamerikbrown@gmail.com>
2014-10-05 00:03:48 -0700

    - Created a new SQL table to hold the list of cities (7 in total at the moment)
    - Greatly changed the SELECT string in results.go to reflect the new database
      changes
    
    - Updated the Listings struct and changed a couple function names to reflect the
      database changes.
    
    - Changed long strings to use the backquote (`) instead of the double quotes.
      This allows us to have multiline strings and not have to escape all the double
      quotes within. The source code also looks much cleaner.
    
    - Created a primitive filter using the query string of in the url (everything
      after the ?). This requires the import of net/url At the moment, o is equal
      to the city origin and d the city destination. The only current valid options
      are o=1&d=2 and o=3&d=5 - This is not a limitation of the code, but a lack of
      listings in the database.

--- go/data.go
+++ go/data.go
@@ -2,29 +2,71 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"data/results"
 	"data/util"
 )
 
-func createHTML (myListing results.Listing) string{
+func generateListing (myListing results.Listing) string{
 	var date util.Date = util.CustomDate(myListing.DateLeaving)
-	output := "<ul class=\"list_item\"><li class=\"listing_user\"><img src=\"http://192.241.219.35/" + myListing.Picture + "\" alt=\"User Picture\"><span class=\"positive\">+100</span></li><li class=\"date_leaving\"><div><span class=\"month\">" + date.Month + "</span><span class=\"month\">" + date.Day + "</span><span class=\"month\">" + date.Time + "</span></div></li><li class=\"city\"><span>" + myListing.Origin + "</span><span class=\"to\">&#10132;</span><span>" + myListing.Destination + "</span></li><li class=\"seats\"><span>2</span></li><li class=\"fee\"><span>$" + fmt.Sprintf("%.2f", myListing.Fee) + "</span></li></ul>"
+	output := `
+	<ul class="list_item">
+		<li class="listing_user">
+			<img src="http://192.241.219.35/` + myListing.Picture + `" alt="User Picture">
+			<span class="positive">+100</span>
+		</li>
+		<li class="date_leaving">
+			<div>
+				<span class="month">` + date.Month + `</span>
+				<span class="day">` + date.Day + `</span>
+				<span class="time">` + date.Time + `</span>
+			</div>
+		</li>
+		<li class="city">
+			<span>` + myListing.Origin + `</span>
+			<span class="to">&#10132;</span>
+			<span>` + myListing.Destination + `</span>
+		</li>
+		<li class="seats">
+			<span>` + fmt.Sprintf("%d", myListing.Seats) + `</span>
+		</li>
+			<li class="fee"><span>$` + fmt.Sprintf("%.2f", myListing.Fee) + `</span>
+		</li>
+	</ul>
+	`
 	return output
 }
 
-func generateHtml(w http.ResponseWriter, r *http.Request) {
-	results := results.ReturnListings() // Make struct to store everything
-	myString := "<!doctype html><html><head><title>Title</title><link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,400italic,600,300,700,800|Bitter:400,400italic,700' rel='stylesheet' type='text/css'><link rel=\"stylesheet\" type=\"text/css\" href=\"http://192.241.219.35/style.css\" /></head><body>"
-	for i := range results{
-		myString += createHTML(results[i])
+func showListings(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		// panic
 	}
-	myString += "</body></html>"
+	m, _ := url.ParseQuery(u.RawQuery)
+	results := results.ReturnListings(m["o"][0], m["d"][0]) // Make struct to store everything
+	myString := `
+	<!doctype html>
+	<html>
+		<head>
+			<title>Title</title>
+			<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,400italic,600,300,700,800|Bitter:400,400italic,700" rel="stylesheet" type="text/css">
+			<link rel="stylesheet" type="text/css" href="http://192.241.219.35/style.css" />
+		</head>
+	<body>
+	`
+	for i := range results{
+		myString += generateListing(results[i])
+	}
+	myString += `
+	</body>
+	</html>
+	`
 	fmt.Fprint(w, myString)
 }
 
 func main() {
-	http.HandleFunc("/go/", generateHtml)
+	http.HandleFunc("/go/l/", showListings)
 	http.ListenAndServe(":8080", nil)
 }
--- go/results/results.go
+++ go/results/results.go
@@ -8,16 +8,16 @@ import (
 var db *sql.DB
 
 type Listing struct {
-	DatePosting string
-	DateLeaving string
-	Driver string
+	Driver int
 	Picture string
+	DateLeaving string
 	Origin string
 	Destination string
+	Seats int
 	Fee float32
 }
 
-func ReturnListings() []Listing {
+func ReturnListings(o string, d string) []Listing {
 	results := make ([]Listing, 0)
 	// The db should be long lived. Do not recreate it unless accessing a different
 	// database. Do not Open() and Close() from a short lived function, just pass in
@@ -38,7 +38,16 @@ func ReturnListings() []Listing {
 	}
 
 	// Always prepare queries to be used multiple times. The parameter placehold is ?
-	stmt, err := db.Prepare("SELECT l.date_posting, l.date_leaving, u.name, u.picture, l.origin, l.destination, l.fee FROM listings AS l JOIN users AS u ON l.driver = u.id LIMIT ?")
+	stmt, err := db.Prepare(`
+		SELECT u.id, u.picture, l.date_leaving, c.name, c2.name, l.seats, l.fee
+			FROM listings AS l
+			JOIN users AS u ON l.driver = u.id
+			JOIN cities AS c ON l.origin = c.id
+			LEFT JOIN cities AS c2 ON l.destination = c2.id
+			WHERE l.origin = ? AND l.destination = ? AND DATE(l.date_leaving) >= '2012-10-01 12:30:00'
+			ORDER BY l.date_leaving
+			LIMIT 25;
+		`)
 	
 	if err != nil {
 		panic(err.Error()) // Have a proper error in production
@@ -47,7 +56,7 @@ func ReturnListings() []Listing {
 
 	// db.Query() prepares, executes, and closes a prepared statement - three round
 	// trips to the databse. Call it infrequently as possible; use efficient SQL statments
-	rows, err := stmt.Query(8)
+	rows, err := stmt.Query(o, d)
 	if err != nil {
 		panic(err.Error()) // Have a proper error in production
 	}
@@ -58,7 +67,7 @@ func ReturnListings() []Listing {
 	// The last rows.Next() call will encounter an EOF error and call rows.Close()
 	for rows.Next() {
 		var temp Listing
-		err := rows.Scan(&temp.DatePosting, &temp.DateLeaving, &temp.Driver, &temp.Picture, &temp.Origin, &temp.Destination, &temp.Fee)
+		err := rows.Scan(&temp.Driver, &temp.Picture, &temp.DateLeaving, &temp.Origin, &temp.Destination, &temp.Seats, &temp.Fee)
 		if err != nil {
 			panic(err.Error()) // Have a proper error in production
 		}
